e425e136693ee56e3c7e1edaf75ede5b8642b736
alfitof <alfito.fbriansyah@gmail.com>
2022-12-01 13:11:01 +0700

    Bug Fixing

--- src/components/Side.js
+++ src/components/Side.js
@@ -1,6 +1,5 @@
 import '../style.css';
 import Searchbar from './Searchbar';
-import Carousel from './Carousel';
 
 const Side = () => {
   return (
@@ -9,10 +8,10 @@ const Side = () => {
 
       <label for="menu-open" id="mobile-menu-button" className="fixed right-2 bottom-2 shadow-lg rounded-full p-2 bg-gray-100 text-gray-600 md:hidden" data-dev-hint="floating action button">
         <svg id="menu-open-icon" className="h-6 w-6 transition duration-200 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
         <svg id="menu-close-icon" className="h-6 w-6 transition duration-200 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </label>
 
@@ -32,7 +31,7 @@ const Side = () => {
             </a>
             <a href="#" className="flex items-center py-2 pl-7 p-2 text-base font-medium text-gray-500 no-underline rounded-lg hover:bg-gray-200">
               <button className="bg-transparent border border-gray-500 rounded-lg border-2 p-1.5">
-                <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   {' '}
                   <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" /> <circle cx="9" cy="7" r="4" /> <path d="M23 21v-2a4 4 0 0 0-3-3.87" /> <path d="M16 3.13a4 4 0 0 1 0 7.75" />
                 </svg>
@@ -46,18 +45,18 @@ const Side = () => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   {' '}
                   <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <line x1="3" y1="6" x2="3" y2="19" /> <line x1="12" y1="6" x2="12" y2="19" />{' '}
                   <line x1="21" y1="6" x2="21" y2="19" />
                 </svg>
               </button>
-              <span className="flex-1 ml-3 whitespace-nowrap">ASE Research</span>
+              <span className="flex-1 ml-3 whitespace-nowrap ">ASE Research</span>
             </a>
             <a href="#" className="flex items-center py-2 pl-7 p-2 my-2 text-base font-medium text-gray-500 no-underline rounded-lg hover:bg-gray-200">
               <button className="bg-transparent border border-gray-500 rounded-lg border-2 p-1.5">
@@ -66,11 +65,11 @@ const Side = () => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   {' '}
                   <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <line x1="3" y1="6" x2="3" y2="19" /> <line x1="12" y1="6" x2="12" y2="19" />{' '}
@@ -86,11 +85,11 @@ const Side = () => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   {' '}
                   <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <line x1="3" y1="6" x2="3" y2="19" /> <line x1="12" y1="6" x2="12" y2="19" />{' '}
@@ -106,11 +105,11 @@ const Side = () => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   {' '}
                   <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <line x1="3" y1="6" x2="3" y2="19" /> <line x1="12" y1="6" x2="12" y2="19" />{' '}
@@ -126,11 +125,11 @@ const Side = () => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   {' '}
                   <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /> <line x1="3" y1="6" x2="3" y2="19" /> <line x1="12" y1="6" x2="12" y2="19" />{' '}
