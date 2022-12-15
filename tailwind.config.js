14f23a61aa29dd78978736709057551a66fde892
Danu Tryas <Danupristowo09@gmail.com>
2022-12-15 13:38:11 +0700

    [add] extends color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        orangeAccent: {
+          100 : "#FEEAD2",
+          200 : "#FDD0A7",
+          300 : "#FAAE7A",
+          400 : "#F68C58",
+          500 : "#F15A25",
+          600 : "#D03D1B",
+          700 : "#AD2412",
+          800 : "#8B120A",
+          900 : "#730609",
+        },
+      }
+    },
   },
   plugins: [],
 };
